c405d9ee0a52a4b51a2b26070effe8664bfb8e9c
ajorgensen89 <ajorgensen89@gmail.com>
2023-09-04 14:35:29 +0000

    nav bar and favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Moments.</h1>{' '}
       <Button variant="success">Success</Button>{' '}
     </div>
