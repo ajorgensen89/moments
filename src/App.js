3a07a7a531c594eb6c58b0b32a0f2f7f0fc82760
ajorgensen89 <ajorgensen89@gmail.com>
2023-09-04 13:27:55 +0000

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <h1>Moments.</h1>>
+      <h1>Moments.</h1>{' '}
+      <Button variant="success">Success</Button>{' '}
     </div>
   );
 }
