fcf2f78a90f32121661817906dd751417d55eca9
ajorgensen89 <ajorgensen89@gmail.com>
2023-10-27 14:44:29 +0000

    sign up before handling errors and events

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import styles from "./styles/App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <h1>Page not found!</h1>} />
         </Switch>
       </Container>
