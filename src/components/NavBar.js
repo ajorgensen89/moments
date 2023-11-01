d85542123624eca144d078439cbc3ed8d88b3c59
ajorgensen89 <ajorgensen89@gmail.com>
2023-11-01 16:32:16 +0000

    Sign in and Sign up forms. Event handles/listeners. Hooks and tidied.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,112 @@
-import React from "react";
+import React from 'react'
+// import React, { useEffect, useRef, useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
-/** Ensures anchoring for NavLinks */
 import { NavLink } from "react-router-dom";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 const NavBar = () => {
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+
+  const {expanded, setExpanded, ref} = useClickOutsideToggle();
+
+  // const [expanded, setExpanded] = useState(false);
+  // const ref = useRef(null);
+  // useEffect(() => {
+  //   const handleClickOutside = (event) => {
+  //     if (ref.current && !ref.current.contains(event.target)) {
+  //       setExpanded(false)
+  //     }
+  //   }
+
+  //   document.addEventListener('mouseup', handleClickOutside)
+  //   return () => {
+  //     document.removeEventListener('mouseup', handleClickOutside)
+  //   }
+  // }, {ref})
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addPostIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/posts/create"
+    >
+      <i className="far fa-plus-square"></i>Add post
+    </NavLink>
+  );
+  const loggedInIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/feed"
+      >
+        <i className="fas fa-stream"></i>Feed
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/liked"
+      >
+        <i className="fas fa-heart"></i>Liked
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+        <i className="fas fa-sign-out-alt"></i>Sign out
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+    </>
+  );
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
+
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {currentUser && addPostIcon}
+        <Navbar.Toggle ref={ref} onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
@@ -25,20 +117,8 @@ const NavBar = () => {
             >
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <NavLink
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/signin"
-            >
-              <i className="fas fa-sign-in-alt"></i>Sign in
-            </NavLink>
-            <NavLink
-              to="/signup"
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-            >
-              <i className="fas fa-user-plus"></i>Sign up
-            </NavLink>
+
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -48,43 +128,106 @@ const NavBar = () => {
 
 export default NavBar;
 
-// import React from "react";
-// import { Navbar, Container, Nav, NavDropdown, Form, FormControl } from "react-bootstrap";
-// import Button from 'react-bootstrap/Button';
-// import logo from '../assets/logo.png'
+// // import React, { useContext } from "react";
+// import { Navbar, Container, Nav } from "react-bootstrap";
+// import logo from "../assets/logo.png";
 // import styles from "../styles/NavBar.module.css";
+// /** Ensures anchoring for NavLinks */
 // import { NavLink } from "react-router-dom";
-
+// // import { CurrentUserContext } from "../App";
+// import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 // const NavBar = () => {
-//     /** className={styles.NavBar}  can be in NavBar or Container - wouldnt work in NavBar in NavBar.module.css*/
-//     return <Navbar className={styles.NavBar} bg="light" expand="md" fixed="top">
+//   // was useContext(CurrentUserContext)
+//   const currentUser = useCurrentUser();
+//   const loggedInIcons = <>{currentUser?.username}</>
+//   const loggedOutIcons = (
+//     <>
+//       <NavLink
+//         className={styles.NavLink}
+//         activeClassName={styles.Active}
+//         to="/signin"
+//       >
+//         <i className="fas fa-sign-in-alt"></i>Sign in
+//       </NavLink>
+//       <NavLink
+//         to="/signup"
+//         className={styles.NavLink}
+//         activeClassName={styles.Active}
+//       >
+//         <i className="fas fa-user-plus"></i>Sign up
+//       </NavLink>
+//     </>
+//   )
 
-//         <Container className={styles.NavBar}>
-//             <NavLink to="/">
-//             <Navbar.Brand><img src={logo} alt="logo" height="45" />Exotic Fruit</Navbar.Brand>
+//   return (
+//     <Navbar className={styles.NavBar} expand="md" fixed="top">
+//       <Container>
+//         <NavLink to="/">
+//           <Navbar.Brand>
+//             <img src={logo} alt="logo" height="45" />
+//           </Navbar.Brand>
+//         </NavLink>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="ml-auto text-left">
+//             <NavLink
+//               exact
+//               className={styles.NavLink}
+//               activeClassName={styles.Active}
+//               to="/"
+//             >
+//               <i className="fas fa-home"></i>Home
 //             </NavLink>
-//             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//             <Navbar.Collapse id="basic-navbar-nav">
-//                 <Nav className="ml-auto">
-//                     <NavLink exact className={styles.NavLink} activeClassStyles={styles.Active} to="/"><i className="fas fa-home"></i> Home</NavLink>
-//                     <NavLink className={styles.NavLink} activeClassStyles={styles.Active} to="/signin"><i className="fas fa-arrow-right"></i> Sign in</NavLink>
-//                     <NavLink className={styles.NavLink} activeClassStyles={styles.Active} to="/signup"><i className="fa-solid fa-check"></i> Sign up</NavLink>
-//                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-//                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-//                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-//                         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-//                         <NavDropdown.Divider />
-//                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-//                     </NavDropdown>
-//                 </Nav>
-//                 <Form inline>
-//                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-//                     <Button variant="outline-success">Search</Button>
-//                 </Form>
-//             </Navbar.Collapse>
-//         </Container>
+            
+//             {currentUser ? loggedInIcons : loggedOutIcons}
+
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
 //     </Navbar>
-// }
+//   );
+// };
 
 // export default NavBar;
+
+// // import React from "react";
+// // import { Navbar, Container, Nav, NavDropdown, Form, FormControl } from "react-bootstrap";
+// // import Button from 'react-bootstrap/Button';
+// // import logo from '../assets/logo.png'
+// // import styles from "../styles/NavBar.module.css";
+// // import { NavLink } from "react-router-dom";
+
+
+// // const NavBar = () => {
+// //     /** className={styles.NavBar}  can be in NavBar or Container - wouldnt work in NavBar in NavBar.module.css*/
+// //     return <Navbar className={styles.NavBar} bg="light" expand="md" fixed="top">
+
+// //         <Container className={styles.NavBar}>
+// //             <NavLink to="/">
+// //             <Navbar.Brand><img src={logo} alt="logo" height="45" />Exotic Fruit</Navbar.Brand>
+// //             </NavLink>
+// //             <Navbar.Toggle aria-controls="basic-navbar-nav" />
+// //             <Navbar.Collapse id="basic-navbar-nav">
+// //                 <Nav className="ml-auto">
+// //                     <NavLink exact className={styles.NavLink} activeClassStyles={styles.Active} to="/"><i className="fas fa-home"></i> Home</NavLink>
+// //                     <NavLink className={styles.NavLink} activeClassStyles={styles.Active} to="/signin"><i className="fas fa-arrow-right"></i> Sign in</NavLink>
+// //                     <NavLink className={styles.NavLink} activeClassStyles={styles.Active} to="/signup"><i className="fa-solid fa-check"></i> Sign up</NavLink>
+// //                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+// //                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+// //                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+// //                         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+// //                         <NavDropdown.Divider />
+// //                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+// //                     </NavDropdown>
+// //                 </Nav>
+// //                 <Form inline>
+// //                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+// //                     <Button variant="outline-success">Search</Button>
+// //                 </Form>
+// //             </Navbar.Collapse>
+// //         </Container>
+// //     </Navbar>
+// // }
+
+// // export default NavBar;
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,13 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../styles/App.module.css";
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+
+import axios from "axios";
 
 const SignUpForm = () => {
+    const [signUpData, setSignUpData] = useState({
+        username: "",
+        password1: "",
+        password2: "",
+    });
+    const { username, password1, password2 } = signUpData;
+
+    const [errors, setErrors] = useState({});
+
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setSignUpData({
+            ...signUpData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("/dj-rest-auth/registration/", signUpData);
+            history.push("/signin");
+        } catch (err) {
+            setErrors(err.response?.data);
+        }
+    };
+
+
     return (
         <Row className={styles.Row}>
             <Col className="my-auto py-2 p-md-2" md={6}>
@@ -16,41 +47,68 @@ const SignUpForm = () => {
 
                     {/* add your form here */}
 
-                    <Form>
+                    <Form onSubmit={handleSubmit} >
                         <Form.Group controlId="username">
                             <Form.Label className="d-none">Username</Form.Label>
                             <Form.Control
-                            className={styles.Input}
+                                className={styles.Input}
                                 type="text"
                                 placeholder="Username"
                                 name="username"
+                                value={username}
+                                onChange={handleChange}
                             />
 
                         </Form.Group>
+                        {errors.username?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
 
                         <Form.Group controlId="password1">
                             <Form.Label className="d-none">Password</Form.Label>
                             <Form.Control
-                            className={styles.Input}
+                                className={styles.Input}
                                 type="password"
                                 placeholder="Confirm Password"
                                 name="password1"
+                                value={password1}
+                                onChange={handleChange}
                             />
                         </Form.Group>
+                        {errors.password1?.map((message, idx) => (
+                            <Alert key={idx} variant="warning">
+                                {message}
+                            </Alert>
+                        ))}
 
                         <Form.Group controlId="password2">
                             <Form.Label className="d-none">Confirm Password</Form.Label>
                             <Form.Control
-                            className={styles.Input}
+                                className={styles.Input}
                                 type="password"
                                 placeholder="Password"
                                 name="password2"
+                                value={password2}
+                                onChange={handleChange}
                             />
                         </Form.Group>
+                        {errors.password2?.map((message, idx) => (
+                            <Alert key={idx} variant="warning">
+                                {message}
+                            </Alert>
+                        ))}
 
                         <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
                             SIgn In
                         </Button>
+                        {errors.non_field_errors?.map((message, idx) => (
+                            <Alert key={idx} variant="warning" className="mt-3">
+                                {message}
+                            </Alert>
+                        ))}
+
                     </Form>
 
                 </Container>
