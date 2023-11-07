da589a98056b6c49fd34d263e88ffc906c5bc805
ajorgensen89 <ajorgensen89@gmail.com>
2023-11-07 15:05:59 +0000

    Starting to comment

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
+import PostEditForm from "./pages/posts/PostEditForm";
 // import { createContext, useEffect, useState } from "react";
 // import axios from "axios";
 
@@ -66,6 +67,7 @@ function App() {
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
+          <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -8,19 +8,30 @@ import appStyles from "../../styles/App.module.css";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import Comment from "../comments/Comment";
+
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostPage() {
     const { id } = useParams();
     const [post, setPost] = useState({ results: [] });
 
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
+
     useEffect(() => {
         const handleMount = async () => {
             try {
-                const [{ data: post }] = await Promise.all([
+                const [{ data: post }, { data: comments }] = await Promise.all([
                     axiosReq.get(`/posts/${id}`),
+                    axiosReq.get(`/comments/?post=${id}`)
                 ]);
                 setPost({ results: [post] });
+                setComments(comments);
                 console.log(post);
+                console.log(comments);
 
             } catch (err) {
                 console.log(err);
@@ -35,9 +46,28 @@ function PostPage() {
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <p>Popular profiles for mobile</p>
                 {/* <p>Post component</p> */}
-                <Post {...post.results[0] } setPosts={setPost} postPage />
+                <Post {...post.results[0]} setPosts={setPost} postPage />
                 <Container className={appStyles.Content}>
-                    Comments
+                    {currentUser ? (
+                        <CommentCreateForm
+                            profile_id={currentUser.profile_id}
+                            profileImage={profile_image}
+                            post={id}
+                            setPost={setPost}
+                            setComments={setComments}
+                        />
+                    ) : comments.results.length ? (
+                        "Comments"
+                    ) : null}
+                    {comments.results.length ? (
+                        comments.results.map(comment => (
+                            <Comment  key={comment.id} {...comment}/>
+                        ))
+                    ) : currentUser ? (
+                        <span>Start a convo about this post here...</span>
+                    ) : (
+                        <span>No Comments yet...</span>
+                    )}
                 </Container>
             </Col>
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
