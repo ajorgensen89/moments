796292380505def5900301cca8744e7bd065a401
ajorgensen89 <ajorgensen89@gmail.com>
2023-11-08 16:52:46 +0000

    start code for Profile page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
+import ProfilePage from "./pages/profiles/ProfilePage";
+
 // import { createContext, useEffect, useState } from "react";
 // import axios from "axios";
 
@@ -68,6 +70,7 @@ function App() {
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
+          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
