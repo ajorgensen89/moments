c99eea0f70611694812e384d3bba511729252d34
ajorgensen89 <ajorgensen89@gmail.com>
2023-11-08 15:43:57 +0000

    sort out profile and restructer. Add and change into components

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,36 +1,44 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+// import { Container } from "react-bootstrap";
+// import { axiosReq } from "../../api/axiosDefaults";
+// import appStyles from "../../styles/App.module.css";
+// import Asset from "../../components/Asset";
+// import { useCurrentUser } from "../../contexts/CurrentUserContext";
+// import Profile from "./Profile";
+import React from "react";
 import { Container } from "react-bootstrap";
-import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../styles/App.module.css";
 import Asset from "../../components/Asset";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useProfileData } from "../../contexts/ProfileDataContext";
+import Profile from "./Profile";
 
 const PopularProfiles = ({ mobile }) => {
-    const [profileData, setProfileData] = useState({
-        // we will use the pageProfile later!
-        pageProfile: { results: [] },
-        popularProfiles: { results: [] },
-    });
-    const { popularProfiles } = profileData;
-    const currentUser = useCurrentUser();
+    const { popularProfiles } = useProfileData();
+    //     const [profileData, setProfileData] = useState({
+    //         // we will use the pageProfile later!
+    //         pageProfile: { results: [] },
+    //         popularProfiles: { results: [] },
+    //     });
+    //     const { popularProfiles } = profileData;
+    //     const currentUser = useCurrentUser();
 
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const { data } = await axiosReq.get(
-                    "/profiles/?ordering=-followers_count"
-                );
-                setProfileData((prevState) => ({
-                    ...prevState,
-                    popularProfiles: data,
-                }));
-            } catch (err) {
-                console.log(err);
-            }
-        };
+    //     useEffect(() => {
+    //         const handleMount = async () => {
+    //             try {
+    //                 const { data } = await axiosReq.get(
+    //                     "/profiles/?ordering=-followers_count"
+    //                 );
+    //                 setProfileData((prevState) => ({
+    //                     ...prevState,
+    //                     popularProfiles: data,
+    //                 }));
+    //             } catch (err) {
+    //                 console.log(err);
+    //             }
+    //         };
 
-        handleMount();
-    }, [currentUser]);
+    //         handleMount();
+    //     }, [currentUser]);
 
     return (
         <Container className={`${appStyles.Content} ${mobile && 'd-lg-none text-center mb-3'}`}>
@@ -40,13 +48,13 @@ const PopularProfiles = ({ mobile }) => {
                     {mobile ? (
                         <div className="d-flex justify-content-around" >
                             {popularProfiles.results.slice(0, 4).map((profile) => (
-                                <p key={profile.id}>{profile.owner}</p>
+                                <Profile key={profile.id} profile={profile} mobile />
                             ))}
                         </div>
                     ) :
                         (
                             popularProfiles.results.map((profile) => (
-                                <p key={profile.id}>{profile.owner}</p>
+                                <Profile key={profile.id} profile={profile} />
                             ))
                         )}
 
