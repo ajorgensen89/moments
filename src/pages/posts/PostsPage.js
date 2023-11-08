4726ab83af5a5cb298f04d36ed7b2c10c885df3b
ajorgensen89 <ajorgensen89@gmail.com>
2023-11-08 14:03:30 +0000

    popular profiles and users. Sort out mobile view aswell as desktop

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -14,6 +14,7 @@ import NoResults from "../../assets/no-results.png"
 import Asset from "../../components/Asset";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function PostsPage({ message, filter = "" }) {
     const [posts, setPosts] = useState({ results: [] });
@@ -46,7 +47,7 @@ function PostsPage({ message, filter = "" }) {
     return (
         <Row className="h-100">
             <Col className="py-2 p-0 p-lg-2" lg={8}>
-                <p>Popular profiles mobile</p>
+                <PopularProfiles mobile />
                 <i className={`fas fa-search ${styles.SearchIcon}`} />
                 <Form className={styles.SearchBar}
                     onSubmit={(event) => event.preventDefault()}>
@@ -67,12 +68,12 @@ function PostsPage({ message, filter = "" }) {
                                     posts.results.map(post => (
                                         <Post key={post.id} {...post} setPosts={setPosts} />
                                     ))
-                                } 
+                                }
                                 dataLength={posts.results.length}
                                 loader={<Asset spinner />}
                                 hasMore={!!posts.next}
                                 next={() => fetchMoreData(posts, setPosts)}
-                                />
+                            />
 
                             // console.log("map over posts, render here.")
                         ) : (
@@ -90,7 +91,7 @@ function PostsPage({ message, filter = "" }) {
                 )}
             </Col>
             <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-                <p>Popular profiles for desktop</p>
+                <PopularProfiles />
             </Col>
         </Row>
     );
